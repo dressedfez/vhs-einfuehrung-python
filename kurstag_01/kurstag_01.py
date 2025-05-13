deb88b51e2ec5c659d9e590048168fc406305d27
Frank Zimmer <frank.e.zimmer@gmail.com>
2025-05-13 22:35:36 +0200

    Füge mehre Informationen für Kurstag 1 hinzu

--- kurstag_01/kurstag_01.py
+++ kurstag_01/kurstag_01.py
@@ -1,10 +1,10 @@
 import marimo
 
 __generated_with = "0.13.6"
-app = marimo.App(layout_file="layouts/kurstag_01.slides.json")
+app = marimo.App()
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     import marimo as mo
     return (mo,)
@@ -21,19 +21,71 @@ def _(mo):
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
-    ## Installation
+    # Variablen
 
-    In diesem Kurs benutzen wir als Paketmanager `uv`
+    Was sind Variablen?
 
-    <img src="public/uv_python.png" width=200 class="center">
+    /// note | Definition
+    Variablen sind Verweise auf Speicherorte von Werte, die dort hinterlegt und von dort ausgelesen werden können.
+    ///
 
-    Damit können wir fast alle relevanten Schritte zur Installation, Delinstallation  von Python und Bibliotheken (Packages) vornehmen.
+    Hier ein grafische Darstellung für eine gespeicherte Zahl
+    <img src="./public/int_memory.svg" width="200">
     """
     )
+    return
+
+
+@app.cell
+def _():
+    min_zahl_studenten = 5
+    min_zahl_studenten
+    return
+
+
+app._unparsable_cell(
+    r"""
+    und hier eine grafische Darstellung eines Arrays von Strings (str)
+    <img src=\"./public/str_memory.svg\" width=\"200\">
+    """,
+    column=None, disabled=False, hide_code=True, name="_"
+)
+
+
+@app.cell
+def _():
+    meldung = ["Hallo","Welt"]
+    meldung
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    # Datentypen
+
+    **Was sind Datentypen?**
+
+    /// note | Definition
+    Datentypen sind 
+
+    ///
+
+
+
+    Python unterstützt eine große Zahl bereits vorgefertigter Datentypen
+    """
+    )
+    return
+
+
+@app.cell
+def _():
     return
 
 
