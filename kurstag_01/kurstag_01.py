6500eb922ceb7067bcc740607eef14c11fe68fe0
Frank Zimmer <frank.e.zimmer@gmail.com>
2025-11-29 17:08:51 +0100

    Aktualsiere Kurstag 01

--- kurstag_01/kurstag_01.py
+++ kurstag_01/kurstag_01.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.14.9"
+__generated_with = "0.18.1"
 app = marimo.App()
 
 
@@ -12,19 +12,16 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     # Python für Daten und KI
     ## – Programmieren lernen für die Zukunft -
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     # Variablen
 
     Was sind Variablen?
@@ -35,8 +32,7 @@ def _(mo):
 
     Hier ein grafische Darstellung für eine gespeicherte Zahl
     <img src="./public/int_memory.svg" width="200">
-    """
-    )
+    """)
     return
 
 
@@ -49,11 +45,10 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
-    Python hat einige Konvenntionen und Regeln, die für Variablennamen gelten. Hier zunächst die Regeln: 
+    mo.md(r"""
+    Python hat einige Konvenntionen und Regeln, die für Variablennamen gelten. Hier zunächst die Regeln:
 
-    /// tip | Regeln 
+    /// tip | Regeln
     1. Eine Variable darf nur aus Buchstaben (a-z,A-Z), Ziffern (0-9) und Unterstrichen bestehen.
     2. Eine Variable darf **nicht** mit einer Ziffer beginnen.
     3. Leer- und Sonderzeichen sind **nicht** erlaubt.
@@ -80,8 +75,7 @@ def _(mo):
     3. Für Konstanten (feste Werte) verwendet man **GROẞBUCHSTABEN**:
     z. B. PI = 3.1415
     ///
-    """
-    )
+    """)
     return
 
 
@@ -98,8 +92,7 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     # Datentypen
 
     **Was sind Datentypen?**
@@ -122,7 +115,7 @@ def _(mo):
     | - | - |
     |Text Type |	str |
     | Numeric Types|	int, float, complex|
-    | Boolean Type | 	bool | 
+    | Boolean Type | 	bool |
     | None Type|	NoneType |
     | Sequence Types |	list, tuple, range |
     | Mapping Type |	dict |
@@ -130,20 +123,17 @@ def _(mo):
     | Binary Types |	bytes, bytearray, memoryview|
 
     Weitere Datentypen können von uns definiert werden, mehr dazu später.
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## Zeichenketten-Datentyp (str)
 
     Mit Zeichenketten (str = Strings) lassen sich Buchstaben, Worte und Sätze abspeichern  und manipulieren.
-    """
-    )
+    """)
     return
 
 
@@ -182,7 +172,9 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Interessant sind auch noch formatierte Strings, die wie folgt definiert werden können:""")
+    mo.md(r"""
+    Interessant sind auch noch formatierte Strings, die wie folgt definiert werden können:
+    """)
     return
 
 
@@ -196,14 +188,12 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## Numerische Datentypen
     ### Ganze Zahlen
 
     Sind alles natürlichen Zahlen ${\mathbb N}$, d.h. ...-2,-1,0,1,2,...., wobei der Computer nur einen endlichen Speicher hat und diese deshalb eingeschränkt sind auf einen endlichen Bereich. Bei Python, anders als bei Java, ist die Einschränkung nur durch den RAM-Speicher des benutzten Computer begrenzt. Es gibt keine obere Schranke innerhalb der Programmierspache.
-    """
-    )
+    """)
     return
 
 
@@ -221,7 +211,9 @@ def _(ganze_zahl):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""folgendes zeigt, dass die riesige Zahlen darstellen kann:""")
+    mo.md(r"""
+    folgendes zeigt, dass die riesige Zahlen darstellen kann:
+    """)
     return
 
 
@@ -233,7 +225,9 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""man kann auch einfach von Gleitkommazahlen oder String (siehe nächste Abschnitt) nach in **int** konvertieren:""")
+    mo.md(r"""
+    man kann auch einfach von Gleitkommazahlen oder String (siehe nächste Abschnitt) nach in **int** konvertieren:
+    """)
     return
 
 
@@ -252,19 +246,19 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Operationen mit **int** werden wir am nächsten Kurstag besprechen.""")
+    mo.md(r"""
+    Operationen mit **int** werden wir am nächsten Kurstag besprechen.
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## Gleitkommazahlen
 
     Gleitkommazahlen sind Zahlen, die einen Nachkommaanteil haben. Dies können einfach mit Python erstellt werden.
-    """
-    )
+    """)
     return
 
 
@@ -282,7 +276,9 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Es gibt auch einige bekannte Konstanten, die in Pyhton eingebaut sind:""")
+    mo.md(r"""
+    Es gibt auch einige bekannte Konstanten, die in Pyhton eingebaut sind:
+    """)
     return
 
 
@@ -296,7 +292,9 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Wie bei **int** kann man von Strings und ganzen Zahlen nach Gleitkommazahlen konvertieren:""")
+    mo.md(r"""
+    Wie bei **int** kann man von Strings und ganzen Zahlen nach Gleitkommazahlen konvertieren:
+    """)
     return
 
 
@@ -314,22 +312,19 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## Listen und Mengen (set)
 
     Python kennt einige Datentypen, die es erlauben mehrere Elemente eines **bestimmten** Datentypen zusammenzufassen. Wir stellen hier als wichtigste Vertreter den `List` und `Set`-Type vor.
 
     ### Listen
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     Listen haben Eigenschaften, die Sie von anderen Kontainertypen unterscheiden.
 
     /// tip | Eigenschaften von Listen
@@ -345,20 +340,17 @@ def _(mo):
 
     Hier eine grafische Darstellung eines Liste (Arrays) von Strings (str)
     <img src="./public/str_memory.svg" width="200">
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     #### Erzeugung von Listen:
 
     Eine leere Liste kann auf diese Weisen:
-    """
-    )
+    """)
     return
 
 
@@ -373,39 +365,50 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""und eine Liste, die schon Elemente enthält, kann so erzeugt werden:""")
+    mo.md(r"""
+    und eine Liste, die schon Elemente enthält, kann so erzeugt werden:
+    """)
     return
 
 
 @app.cell
 def _():
-    liste_mit_zeichenketten = ["Hallo", "Welt"] # Liste mit Zeichenketten als Elemente
-    liste_mit_zahlen = [1,2,3] # Liste mit Zahlen als Elemente
-    liste_mit_unterschielicher_datentypen = [1, "Hallo", 3.14] # Liste, die Elemente enthält, die unterschiedliche Datentypen haben
+    liste_mit_zeichenketten = [
+        "Hallo",
+        "Welt",
+    ]  # Liste mit Zeichenketten als Elemente
+    liste_mit_zahlen = [1, 2, 3]  # Liste mit Zahlen als Elemente
+    liste_mit_unterschielicher_datentypen = [
+        1,
+        "Hallo",
+        3.14,
+    ]  # Liste, die Elemente enthält, die unterschiedliche Datentypen haben
     return (liste_mit_zeichenketten,)
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     /// attention | Bemerkung
     Eine Liste kann beliebige Elemente enthalten, die auch nicht unbedingt vom gleichen Typ sein müssen. Im Allgemeinen ist es aber üblich, dass die Elemente den gleichen Datentyp haben.
     ///
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""#### Operationen auf Listen""")
+    mo.md(r"""
+    #### Operationen auf Listen
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Über die **append**-Methode können Elemente an das Ende einer Liste angehängt werden:""")
+    mo.md(r"""
+    Über die **append**-Methode können Elemente an das Ende einer Liste angehängt werden:
+    """)
     return
 
 
@@ -413,26 +416,28 @@ def _(mo):
 def _(liste_mit_zeichenketten):
     liste_mit_zeichenketten.append("und")
     liste_mit_zeichenketten.append("Kurs")
-    liste_mit_zeichenketten.append("Kurs") # ein Element kann auch zwei Mal hinzugefügt werden
+    liste_mit_zeichenketten.append(
+        "Kurs"
+    )  # ein Element kann auch zwei Mal hinzugefügt werden
     liste_mit_zeichenketten
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
-    /// tip 
+    mo.md(r"""
+    /// tip
     `append` hängt Element am Ende der Liste an.
     ///
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""über die **remove**-Methode können Elemente entfernt werden:""")
+    mo.md(r"""
+    über die **remove**-Methode können Elemente entfernt werden:
+    """)
     return
 
 
@@ -445,26 +450,28 @@ def _(liste_mit_zeichenketten):
 
 @app.cell
 def _(liste_mit_zeichenketten):
-    liste_mit_zeichenketten.remove("Kurs") # remove entfernt das erste Element, das in der Liste gefunden wird, wenn man von vorne sucht
+    liste_mit_zeichenketten.remove(
+        "Kurs"
+    )  # remove entfernt das erste Element, das in der Liste gefunden wird, wenn man von vorne sucht
     liste_mit_zeichenketten
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
-    /// tip 
+    mo.md(r"""
+    /// tip
     `remove` entfernt das erste Element, das in der Liste gefunden wird.
     ///
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Zum Entfernen des letzten Elements einer Liste gibt es den Befehl **pop**:""")
+    mo.md(r"""
+    Zum Entfernen des letzten Elements einer Liste gibt es den Befehl **pop**:
+    """)
     return
 
 
@@ -483,37 +490,40 @@ def _(liste_mit_zeichenketten):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     /// tip
     `pop` entfernt ohne weiteres Argument das letzte Element der Liste.
     ///
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     #### Indizieren in Listen
 
-    Dies bedeutet, dass ich Elemente der Liste aus dieser dediziert auslese. Da eine Liste geordnet ist, kann ich jedes Listenelement direkt anspechen. Die Indizierung der Liste beginnt bei 0 und geht bis Länger der Liste minus 1. Hier ein Beispiel: 
-    """
-    )
+    Dies bedeutet, dass ich Elemente der Liste aus dieser dediziert auslese. Da eine Liste geordnet ist, kann ich jedes Listenelement direkt anspechen. Die Indizierung der Liste beginnt bei 0 und geht bis Länger der Liste minus 1. Hier ein Beispiel:
+    """)
     return
 
 
 @app.cell
 def _():
-    indizieren_in_liste = ["erste Element", "zweites Element", "drittes Element", "viertes Element"]
+    indizieren_in_liste = [
+        "erste Element",
+        "zweites Element",
+        "drittes Element",
+        "viertes Element",
+    ]
     return (indizieren_in_liste,)
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Das erste Element erhalte ich, in dem ich auf das Elemnt mit dem Index 0 zugreife: """)
+    mo.md(r"""
+    Das erste Element erhalte ich, in dem ich auf das Elemnt mit dem Index 0 zugreife:
+    """)
     return
 
 
@@ -525,25 +535,29 @@ def _(indizieren_in_liste):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""das letzte Element erhalte ich, in dem ich auf das Element mit dem Index **Länge der Liste minus 1** zugreife: """)
+    mo.md(r"""
+    das letzte Element erhalte ich, in dem ich auf das Element mit dem Index **Länge der Liste minus 1** zugreife:
+    """)
     return
 
 
 @app.cell
 def _(indizieren_in_liste):
-    len(indizieren_in_liste) #Länge der Liste
+    len(indizieren_in_liste)  # Länge der Liste
     return
 
 
 @app.cell
 def _(indizieren_in_liste):
-    indizieren_in_liste[len(indizieren_in_liste)-1]
+    indizieren_in_liste[len(indizieren_in_liste) - 1]
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""andere Möglichkeit ist: """)
+    mo.md(r"""
+    andere Möglichkeit ist:
+    """)
     return
 
 
@@ -555,8 +569,7 @@ def _(indizieren_in_liste):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     /// note | Definition:
     Der Slice-Operator : wird in Python verwendet, um einen zusammenhängenden Teil (Ausschnitt) einer Sequenz wie einer Liste, einem String oder einem Tupel auszuwählen. Mit ihm können Bereiche über Start-, End- und optionalen Schrittwerten spezifiziert werden, sodass ein neuer Teilbereich (Slice) der Sequenz zurückgegeben wird, ohne das Original zu verändern.
 
@@ -572,38 +585,37 @@ def _(mo):
     ///
 
     **Beipiele:**
-    """
-    )
+    """)
     return
 
 
 @app.cell
 def _(indizieren_in_liste):
-    indizieren_in_liste[0:3] 
+    indizieren_in_liste[0:3]
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     /// attention | Achtung
     Bei dem **Slice**-Operator wird der letzte der **stop**-Wert nicht angenommen.
     ///
-    """
-    )
+    """)
     return
 
 
 @app.cell
 def _(indizieren_in_liste):
-    indizieren_in_liste[0:4] 
+    indizieren_in_liste[0:4]
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Mit dem **step** kann man jedes **x**-te Element extrahieren: """)
+    mo.md(r"""
+    Mit dem **step** kann man jedes **x**-te Element extrahieren:
+    """)
     return
 
 
@@ -615,7 +627,9 @@ def _(indizieren_in_liste):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Ist der **step** negativ, wird die Liste rückwärts durchlaufen: """)
+    mo.md(r"""
+    Ist der **step** negativ, wird die Liste rückwärts durchlaufen:
+    """)
     return
 
 
@@ -627,8 +641,7 @@ def _(indizieren_in_liste):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     /// note | Übungen
 
     1. Welche weiteren Operationen kann man mit Listen noch machen (tab-completion)?
@@ -639,27 +652,24 @@ def _(mo):
     ```
     alle Zahlen von 1 bis hundert und extrahiere mittels indizieren alle geraden und ungeraden Zahlen aus dieser Liste.
     ///
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-
     test_wort = "ANNA"
     test_wort
     wort = list(test_wort)
     wort.reverse()
     "".join(wort) == test_wort
-    mo.md(text="# (mögliche) Lösung zu 2.")
+    mo.md(text="(mögliche) Lösung zu 2.")
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-
-    zahlen_eins_bis_100 = list(range(1,101))
+    zahlen_eins_bis_100 = list(range(1, 101))
     ungeraden_zahlen_bis_100 = zahlen_eins_bis_100[0:101:2]
     geraden_zahlen_bis_100 = zahlen_eins_bis_100[1:101:2]
     mo.md("(mögliche) Lösung für 3")
@@ -668,8 +678,7 @@ def _(mo):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Sets/Mengen
 
     Mengen (Sets) haben Eigenschaften, die Sie von anderen Kontainertypen unterscheiden.
@@ -684,38 +693,39 @@ def _(mo):
     ///
 
     Eine leere Menge wird analog zu der Liste mit:
-    """
-    )
+    """)
     return
 
 
 @app.cell
 def _():
-    menge_1 = set() 
+    menge_1 = set()
     return (menge_1,)
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""erzeugt.""")
+    mo.md(r"""
+    erzeugt.
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     /// attention | Achtung
     Eine leere Menge kann **nicht** mit dem Befehl `{}` erzeugt werden. Die Konstruktion  `{}` erzeugt einen leeren Dictionary (Schlüssel-Wert-Paar).
     ///
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Man kann Elemente hinzufügen:""")
+    mo.md(r"""
+    Man kann Elemente hinzufügen:
+    """)
     return
 
 
@@ -728,21 +738,23 @@ def _(menge_1):
 
 @app.cell
 def _(menge_1):
-    menge_1.add("Hallo") # nochmaliges Hinzufügen ist nicht möglich
+    menge_1.add("Hallo")  # nochmaliges Hinzufügen ist nicht möglich
     menge_1
     return
 
 
 @app.cell
 def _():
-    menge_2 = set(['Welt'])
+    menge_2 = set(["Welt"])
     menge_2
     return (menge_2,)
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""### Mengenoperationen""")
+    mo.md(r"""
+    ### Mengenoperationen
+    """)
     return
 
 
@@ -763,29 +775,27 @@ def _(menge_1, vereinigungs_menge):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     /// note | Übungen
     1. Wir haben zwei Listen mit Schülern, die in GTAs gehen wollen, die zur gleichen Zeit stattfinden. Welche Schüler müssen sich nur für eine GTA entscheiden?
-    ```python  
+    ```python
     volleyball = ["Frank", "Egon", "Markus", "Georg", "Frank"]
     fussball = ["Egon", "Franz","Steffi","Markus"]
     ```
     Was machen die Befehle `intersection`, `difference` und `symmetric_difference`?
-    2. Was passiert, wenn man eine Zeichenkette (einen String) in als Argument `set(arg)` 
+    2. Was passiert, wenn man eine Zeichenkette (einen String) in als Argument `set(arg)`
     übergibt?
     3. Bestimme die Buchstaben, die sowohl in
     ```python
     text1 = "Hallo Welt"
     ```
-    also auch 
+    also auch
     ```python
     text2 = "Weltraum"
     ```
     enthalten sind. Welche sind nicht in `text1`, aber in `text2` unumgekehrt enthalten?
     ///
-    """
-    )
+    """)
     return
 
 
